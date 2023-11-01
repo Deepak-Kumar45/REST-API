00150caa8d086a225b75a9fc42252f5298f87794
Deepak Kumar <126335641+Deepak-Kumar45@users.noreply.github.com>
2023-11-01 10:54:13 +0000

    Rest-api2 project has been started

---
+++ rest_api2/src/main/java/com/spring/rest_api2/RestApi2Application.java
@@ -0,0 +1,14 @@
+package com.spring.rest_api2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApi2Application.class, args);
+		System.err.println("\n\t\t!!..Application has been started..!!");
+	}
+
+}
